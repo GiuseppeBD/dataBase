0a8d03c9bce118a3622415c47f19aa5a6a88d618
ShadowBD <bazzano.giuseppe.01@gmail.com>
2023-01-15 18:23:31 +0100

    dettagli

--- nomeDB/src/app/features/dettagli-users/dettagli-user.service.ts
+++ nomeDB/src/app/features/dettagli-users/dettagli-user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { DettagliUser } from 'src/app/core/model/user.model';
+import { Observable } from 'rxjs';
+import { DettagliUser, User } from 'src/app/core/model/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,8 @@ url ="http://localhost:3000/user"
   constructor(private http: HttpClient) { }
 
 
-loadDettagli(){
-return this.http.get<DettagliUser>(this.url)
+loadDettagli():Observable<User[]>{
+return this.http.get<User[]>(this.url)
 }
 
 
--- nomeDB/src/app/features/dettagli-users/dettagli-users.component.ts
+++ nomeDB/src/app/features/dettagli-users/dettagli-users.component.ts
@@ -1,15 +1,21 @@
+import { User } from './../../core/model/user.model';
+import { DettagliUserService } from './dettagli-user.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dettagli-users',
   templateUrl: './dettagli-users.component.html',
-  styleUrls: ['./dettagli-users.component.css']
+  styleUrls: ['./dettagli-users.component.css'],
 })
 export class DettagliUsersComponent {
+  users: User[] = [];
 
+  constructor(private dettUtente: DettagliUserService) {}
 
-
-
-
-
+  ngOnInit() {
+    this.dettUtente.loadDettagli().subscribe((data) => {
+      this.users = data;
+      console.log(this.users)
+    });
+  }
 }
